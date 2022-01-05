bd3e42408b3e9839949b74efae09a400b310680b
Andrew Park <atpark333@gmail.com>
2022-01-05 16:33:03 +0900

    feat: add message client api

--- lib/omi_chan/bocco.ex
+++ lib/omi_chan/bocco.ex
@@ -1,22 +1,34 @@
 defmodule OmiChan.Bocco do
   @base "https://platform-api.bocco.me"
+  @default_headers %{"Content-Type" => "application/json"}
+
+  alias OmiChan.Auth
 
   def refresh_token(token) do
-    req_body = Poison.encode!(%{ refresh_token: token })
-    %{ body: body } = post(get_refresh_uri(), req_body)
-    body = Poison.decode!(body)
-    body
+    {:ok, data} = post(get_refresh_uri(), %{ refresh_token: token })
+    data
   end
 
   defp get_refresh_uri do
     "#{@base}/oauth/token/refresh"
   end
 
-  defp post(uri, body) do
-    response = HTTPoison.post(uri, body, %{ "Content-Type" => "application/json" })
-    case response do
-      {:ok, response} -> response
-      {:error, reason} -> raise "Oh No! #{inspect reason}"
+  def send_message(room_id, message) do
+    token = Auth.get_access_token()
+    auth_header = %{ "Authorization" => "Bearer #{token}" }
+    {:ok, data} = post(get_message_uri(room_id), %{ text: message }, auth_header)
+    data
+  end
+
+  defp get_message_uri(room) do
+    "#{@base}/v1/rooms/#{room}/messages/text"
+  end
+
+  defp post(uri, data, headers \\ %{}) do
+    result = HTTPoison.post(uri, Poison.encode!(data), Map.merge(@default_headers, headers))
+    case result do
+      {:ok, %{ body: body }} -> {:ok, Poison.decode!(body)}
+      {:error, reason} -> raise IO.inspect reason
     end
   end
 end
