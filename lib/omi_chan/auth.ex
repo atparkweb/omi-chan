da4a8bd16b01d8629871a49f6be1501a04fb3cba
Andrew Park <atpark333@gmail.com>
2022-01-05 12:00:59 +0900

    refactor: get token from env and update error message

--- lib/omi_chan/auth.ex
+++ lib/omi_chan/auth.ex
@@ -8,12 +8,17 @@ defmodule OmiChan.Auth do
   defmodule AuthState do
     defstruct refresh_token: nil,
       access_token: nil,
-      interval: :timer.minutes(60) # token expires after 1 hour
+      interval: nil
   end
 
+  @spec start :: :ignore | {:error, any} | {:ok, pid}
   def start do
-    IO.puts "Starting auth server..."
-    GenServer.start(__MODULE__, %AuthState{}, name: @name)
+    refresh = Dotenv.get("REFRESH_TOKEN")
+    {interval, _} = Integer.parse(Dotenv.get("REFRESH_INTERVAL", "3600000")) # default timeout, 60 minutes
+
+    if interval < :timer.minutes(1) do raise "Interval cannot be less than 1 minute" end
+
+    GenServer.start(__MODULE__, %AuthState{refresh_token: refresh, interval: interval}, name: @name)
   end
 
   @impl true
@@ -33,7 +38,7 @@ defmodule OmiChan.Auth do
   @impl true
   def handle_info(:refresh, state) do
     IO.puts "Refreshing token..."
-    %{ "access_token" => access, "refresh_token" => refresh } = refresh_tokens()
+    %{ "access_token" => access, "refresh_token" => refresh } = refresh_tokens(state.refresh_token)
     IO.puts "Access: #{access}, Refresh: #{refresh}"
     new_state = %{ state | access_token: access, refresh_token: refresh }
     schedule_refresh(state.interval)
@@ -44,9 +49,9 @@ defmodule OmiChan.Auth do
     {:noreply, state}
   end
 
-  defp refresh_tokens do
-    Task.async(fn -> Bocco.refresh_token() end)
-      |> Task.await
+  defp refresh_tokens(token) do
+    Task.async(fn -> Bocco.refresh_token(token) end)
+    |> Task.await
   end
 
   defp schedule_refresh(time_in_ms) do
--- lib/omi_chan/bocco.ex
+++ lib/omi_chan/bocco.ex
@@ -1,8 +1,8 @@
 defmodule OmiChan.Bocco do
   @base "https://platform-api.bocco.me"
 
-  def refresh_token do
-    req_body = Poison.encode!(%{ refresh_token: "7f68b74b-33b6-426f-b87f-b5a6f10946a2" })
+  def refresh_token(token) do
+    req_body = Poison.encode!(%{ refresh_token: token })
     %{ body: body } = post(get_refresh_uri(), req_body)
     body = Poison.decode!(body)
     body
@@ -16,7 +16,7 @@ defmodule OmiChan.Bocco do
     response = HTTPoison.post(uri, body, %{ "Content-Type" => "application/json" })
     case response do
       {:ok, response} -> response
-      {:error, _reason} -> raise "Oh No!"
+      {:error, reason} -> raise "Oh No! #{inspect reason}"
     end
   end
 end
